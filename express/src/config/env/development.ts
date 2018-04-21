988bc4852c44c8b8508fd94d20fa3e91f17218e7
Francis Acheampong <francisacheampong@gmail.com>
2018-04-21 10:44:17 -0400

    Heroku and Azure

---
+++ express/src/config/env/development.ts
@@ -0,0 +1,6 @@
+import {AppConfig} from '../app-config';
+
+module.exports = <AppConfig>{
+    db: 'mongodb://francis:comp308@ds014368.mlab.com:14368/mean-ts-dev',
+    sessionSecret: 'developmentSessionSecret'
+};
